81288a0f04dd827df47fafa78992d92aa077ccac
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-12-08 10:24:49 -0500

    Demonstrate problem

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-style-import-test-no-embroider/config/environment';
 
+import './test.css';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
